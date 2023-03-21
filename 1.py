bc2427715dd2ddd5b98ff824d55d28f38a9572b0
Mihail_Vik <drimer-gt@ya.ru>
2023-03-21 17:55:31 +0400

    21.03 калькулятор
    new file:   1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+print ("только для целых чисел")
+znak = input ("выбери операцию '(+, -, *)'")
+a = float (input ("первое число "))
+b = float -(input ("втрое число "))
+if  znak == "+":
+   c = a+b
+elif znak == "-":
+   c = a-b
+elif znak == "*":
+   c = a*b
+   print ("ответ" + str(c)) 
+elif znak == "*"
+   print ("ответ" + str())
+
